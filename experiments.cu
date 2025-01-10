0a494e63dfda5fcc54d0d2a3fdc78d82d84aabf3
magstn <lilsh2023@shanghaitech.edu.cn>
2025-01-10 18:24:24 +0800

    add iters

--- experiments.cu
+++ experiments.cu
@@ -41,22 +41,25 @@ void simpleDemo()
 
 void experiment1(uint32_t t)
 {
-    for (uint32_t exp = 3; exp <= 24; ++exp)
+    for (uint32_t exp = 10; exp <= 24; ++exp)
     {
-        uint32_t n = pow(2, exp);
-        uint32_t size = pow(2, 25);
-        uint32_t maxIter = 4 * log2(n);
-        vector<uint32_t> keys;
-        generateRandomKeys(keys, n);
+        for (int iter = 0; iter < 5; iter++)
+        {
+            uint32_t n = pow(2, exp);
+            uint32_t size = pow(2, 25);
+            uint32_t maxIter = 4 * log2(n);
+            vector<uint32_t> keys;
+            generateRandomKeys(keys, n);
 
-        uint32_t rehashesSeq = 0;
-        SequentialHash sh(size, t, maxIter);
-        auto start = high_resolution_clock::now();
-        sh.insertKeys(keys.data(), n, rehashesSeq);
-        auto end = high_resolution_clock::now();
-        auto durationSeq = duration_cast<microseconds>(end - start).count();
+            uint32_t rehashesSeq = 0;
+            SequentialHash sh(size, t, maxIter);
+            auto start = high_resolution_clock::now();
+            sh.insertKeys(keys.data(), n, rehashesSeq);
+            auto end = high_resolution_clock::now();
+            auto durationSeq = duration_cast<microseconds>(end - start).count();
 
-        printf("E1 t=%u exp=%u [Sequential] %8ld us, rehashes: %4u \n", t, exp, durationSeq, rehashesSeq);
+            printf("E1-%d t=%u exp=%u [Sequential] %8ld us, rehashes: %4u \n", iter, t, exp, durationSeq, rehashesSeq);
+        }
     }
 }
 
@@ -75,22 +78,25 @@ void experiment2(uint32_t t)
 
     for (uint32_t i = 0; i <= 10; ++i)
     {
-        generateRandomKeys(S[i], n * i / 10);
-        // Fill remaining S[i] with keys randomly chosen in keys[]
-        for (uint32_t j = S[i].size(); j < n; ++j)
+        for (int iter = 0; iter < 5; iter++)
         {
-            // Pick a random index from [0, n-1]
-            uint32_t idx = rand() % n;
-            S[i].push_back(keys[idx]);
+            generateRandomKeys(S[i], n * i / 10);
+            // Fill remaining S[i] with keys randomly chosen in keys[]
+            for (uint32_t j = S[i].size(); j < n; ++j)
+            {
+                // Pick a random index from [0, n-1]
+                uint32_t idx = rand() % n;
+                S[i].push_back(keys[idx]);
+            }
+
+            auto start = high_resolution_clock::now();
+            for (uint32_t k = 0; k < n; ++k)
+                sh.lookupKey(S[i][k]);
+            auto end = high_resolution_clock::now();
+            auto durationSeq = duration_cast<microseconds>(end - start).count();
+
+            printf("E2-%d t=%u i=%u [Sequential] %8ld us\n", iter, t, i, durationSeq);
         }
-
-        auto start = high_resolution_clock::now();
-        for (uint32_t k = 0; k < n; ++k)
-            sh.lookupKey(S[i][k]);
-        auto end = high_resolution_clock::now();
-        auto durationSeq = duration_cast<microseconds>(end - start).count();
-
-        printf("E2 t=%u i=%u [Sequential] %8ld us\n", t, i, durationSeq);
     }
 }
 
@@ -105,15 +111,18 @@ void experiment3(uint32_t t)
 
     for (double i : alpha)
     {
-        uint32_t size = (uint32_t)(i * n);
-        SequentialHash sh(size, t, maxIter);
-        uint32_t rehashesSeq = 0;
-        auto start = high_resolution_clock::now();
-        sh.insertKeys(keys.data(), n, rehashesSeq);
-        auto end = high_resolution_clock::now();
-        auto durationSeq = duration_cast<microseconds>(end - start).count();
+        for (int iter = 0; iter < 5; iter++)
+        {
+            uint32_t size = (uint32_t)(i * n);
+            SequentialHash sh(size, t, maxIter);
+            uint32_t rehashesSeq = 0;
+            auto start = high_resolution_clock::now();
+            sh.insertKeys(keys.data(), n, rehashesSeq);
+            auto end = high_resolution_clock::now();
+            auto durationSeq = duration_cast<microseconds>(end - start).count();
 
-        printf("E3 t=%u size=%.2lfn [Sequential] %8ld us, rehashes: %4u\n", t, i, durationSeq, rehashesSeq);
+            printf("E3-%d t=%u size=%.2lfn [Sequential] %8ld us, rehashes: %4u\n", iter, t, i, durationSeq, rehashesSeq);
+        }
     }
 }
 
@@ -129,21 +138,24 @@ void experiment4(uint32_t t)
 
     for (int i = 2; i <= 10; i++)
     {
-        uint32_t maxIter = i * log2(n);
-        SequentialHash sh(size, t, maxIter);
-        uint32_t rehashesSeq = 0;
-        auto start = high_resolution_clock::now();
-        sh.insertKeys(keys.data(), n, rehashesSeq);
-        auto end = high_resolution_clock::now();
-        auto durationSeq = duration_cast<microseconds>(end - start).count();
-
-        if (durationSeq < bestTime)
+        for (int iter = 0; iter < 5; iter++)
         {
-            bestTime = durationSeq;
-            bestAlpha = i;
-        }
+            uint32_t maxIter = i * log2(n);
+            SequentialHash sh(size, t, maxIter);
+            uint32_t rehashesSeq = 0;
+            auto start = high_resolution_clock::now();
+            sh.insertKeys(keys.data(), n, rehashesSeq);
+            auto end = high_resolution_clock::now();
+            auto durationSeq = duration_cast<microseconds>(end - start).count();
 
-        printf("E4 t=%u maxIter=%dlogn [Sequential] %8ld us, rehashes: %4u\n", t, i, durationSeq, rehashesSeq);
+            if (durationSeq < bestTime)
+            {
+                bestTime = durationSeq;
+                bestAlpha = i;
+            }
+
+            printf("E4-%d t=%u maxIter=%dlogn [Sequential] %8ld us, rehashes: %4u\n", iter, t, i, durationSeq, rehashesSeq);
+        }
     }
 
     printf("Best maxIter=%dlogn\n", bestAlpha);
@@ -153,11 +165,11 @@ int main()
 {
     simpleDemo();
 
-    printf ("Experiment 1:\n");
+    printf("Experiment 1:\n");
     experiment1(2);
     experiment1(3);
 
-    printf ("Experiment 2:\n");
+    printf("Experiment 2:\n");
     experiment2(2);
     experiment2(3);
 
@@ -165,7 +177,7 @@ int main()
     experiment3(2);
     experiment3(3);
 
-    printf ("Experiment4:\n");
+    printf("Experiment4:\n");
     experiment4(2);
     experiment4(3);
 
